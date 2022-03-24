d647308669b58242b7a0c406e741d792af8384e6
kaushikpaul <kaushik.paul755@gmail.com>
2022-03-24 14:52:14 +0530

    Add BinarySearch Recursive in Searching module

--- Searching/src/io/algoexpert/easy/BinarySearch.java
+++ Searching/src/io/algoexpert/easy/BinarySearch.java
@@ -2,7 +2,7 @@ package io.algoexpert.easy;
 
 public class BinarySearch {
 
-    public static boolean binarySearch(int[] array, int target) {
+    public static boolean binarySearchIterative(int[] array, int target) {
         int left = 0;
         int right = array.length - 1;
         while (left <= right) {
@@ -18,9 +18,28 @@ public class BinarySearch {
         return false;
     }
 
+    public static boolean binarySearchRecursive(int[] array, int target) {
+        return binarySearchRecursiveHelper(array, target, 0, array.length - 1);
+    }
+
+    private static boolean binarySearchRecursiveHelper(int[] array, int target, int left, int right) {
+        if (left > right) {
+            return false;
+        }
+        int middle = (left + right) / 2;
+        if (array[middle] == target) {
+            return true;
+        }
+        if (array[middle] < target) {
+            return binarySearchRecursiveHelper(array, target, middle + 1, right);
+        } else {
+            return binarySearchRecursiveHelper(array, target, left, middle - 1);
+        }
+    }
+
     public static void main(String[] args) {
         int[] array = new int[]{0, 1, 21, 33, 45, 45, 61, 71, 72, 73};
         int target = 33;
-        System.out.println("Is the number present: " + binarySearch(array, target));
+        System.out.println("Is the number present: " + binarySearchRecursive(array, target));
     }
 }
