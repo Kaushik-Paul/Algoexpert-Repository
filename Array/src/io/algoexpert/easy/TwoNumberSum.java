f514aef442787565f6724f9b20f50ceb71279639
kaushikpaul <kaushik.paul755@gmail.com>
2022-03-24 13:56:50 +0530

    Add two Number sum

--- Array/src/io/algoexpert/easy/TwoNumberSum.java
+++ Array/src/io/algoexpert/easy/TwoNumberSum.java
@@ -1,4 +1,27 @@
 package io.algoexpert.easy;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class TwoNumberSum {
+
+    public static int[] getTwoNumberSum(int[] array, int target) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : array) {
+            int diff = target - num;
+            if (set.contains(diff)) {
+                return new int[]{num, diff};
+            } else {
+                set.add(num);
+            }
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{3, 5, -4, 8, 11, 1, -1, 6};
+        int target = 10;
+        int[] value = getTwoNumberSum(array, target);
+        System.out.println("The two number sums are: " + Arrays.toString(value));
+    }
 }
